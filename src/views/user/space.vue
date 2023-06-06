e1f5cc3d5d098d9ca04ddd851d6a3a6975fff729
AllocBlock <690051933@qq.com>
2023-06-06 23:41:14 +0800

    添加空间页面的曲谱搜索（子串、模糊、大小写不敏感）

--- src/views/user/space.vue
+++ src/views/user/space.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="main">
-    <p style="font-size:40px;">曲谱列表</p>
+    <div style="font-size:40px; margin: 20px 0;">曲谱列表</div>
+    <input type="text" placeholder="搜索曲谱..." v-model="searchText" @input="onSearch"/>
     <div id="sheet-table">
-      <div v-for="project in projectInfos" class="sheet_block button" @click="openProject(project)">
-        <div class="sheet_name">{{ project.sheetMeta.title }}</div>
-        <div class="sheet_singer">—— {{ project.sheetMeta.singer }}</div>
+      <div v-if="projectInfos.length == 0">
+        未找到任何曲谱...
       </div>
+      <template v-else>
+        <div v-for="project in projectInfos" class="sheet_block button" @click="openProject(project)">
+          <div class="sheet_name">{{ project.sheetMeta.title }}</div>
+          <div class="sheet_singer">—— {{ project.sheetMeta.singer }}</div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -18,17 +24,57 @@ export default {
   data() {
     return {
       title: "加载中...",
+      allProjectInfos: [],
       projectInfos: [],
+      searchText: ''
     }
   },
   mounted() {
-    this.projectInfos = Project.getAll()
-    console.log(this.projectInfos)
+    this.allProjectInfos = Project.getAll()
+    this.projectInfos = this.allProjectInfos
+    console.log(this.allProjectInfos)
   },
   methods: {
     openProject(project) {
       window.location.href = `/sheet?pid=${project.pid}`
     },
+    onSearch() {
+      const searchText = this.searchText.toLowerCase()
+      if (!searchText) this.projectInfos = this.allProjectInfos
+      else {
+        let priorities = this.allProjectInfos.map(project => {
+          let priority = 0
+          
+          const title = project.sheetMeta.title.toLowerCase()
+          const singer = project.sheetMeta.singer.toLowerCase()
+          if (title.indexOf(searchText) > -1) {
+            priority = 10000
+          }
+          else if (singer.indexOf(searchText) > -1) {
+            priority = 8000
+          }
+          else {
+            // 模糊搜索
+            for (let char in searchText) {
+              if (title.indexOf(char) > -1) priority++;
+              if (singer.indexOf(char) > -1) priority++;
+            }
+          }
+          return priority
+        })
+
+        let packed = []
+        for (let i = 0; i < priorities.length; ++i) {
+          if (priorities[i] > 0) {
+            packed.push([this.allProjectInfos[i], priorities[i]])
+          }
+        }
+
+        packed.sort((a, b) => a[1] > b[1])
+        
+        this.projectInfos = packed.map(e => e[0])
+      }
+    }
   }
 }
 </script>
